ab501f9b571ef1d347c1123c989a2993dd5f611e
siscon <1748329745@qq.com>
2022-04-02 22:50:03 +0800

    update readme
    also removed unused code

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-
+# 如何使用
+```c#
+static async Task Main(string[] args)
+{
+   Malody malody = new Malody(查分账号使用的账号, 查分账号使用的密码);//Malody malody = new Malody(查分账号使用的账号, 查分账号使用的密码,Malody网站的基本Url（可选）,请求时Header中UserAgent（可选）);
+   var queryList = await ma.MalodyUserQuery("siscon");//查询名字包含siscon字样的用户list
+   var userRecentScore = await ma.SearchRecent(308032);//查询malody id为308032的最近游玩歌曲信息 
+   var user = await ma.GetUserInfo(308032);//查询malody id为308032的用户信息
+   var xCsrfToken = ma.GetXCsrfToken();//获取查分账号的xCsrfToken
+   var sessionId = ma.GetSessionId();//获取查分账号的sessionId
+   var xCsrfTokenExpireDate = ma.GetXCsrfTokenExpireDate();//获取查分账号的xCsrfToken过期时间
+   var sessionIdExpireDate = ma.GetSessionIdExpireDate();//获取查分账号的sessionId过期时间
+}
+```
--- Utils/Network.cs
+++ Utils/Network.cs
@@ -9,68 +9,6 @@ namespace MalodyInfoQuery.Utils
 {
     internal class Network
     {
-        public static async Task<byte[]> Download(string url, Dictionary<string, string> header = null,
-            int timeout = 8000, int limitLen = 0)
-        {
-            // Create request
-            var request = WebRequest.CreateHttp(url);
-            {
-                request.Timeout = timeout;
-                request.ReadWriteTimeout = timeout;
-                request.AutomaticDecompression = DecompressionMethods.All;
-
-                // Default useragent
-                request.Headers.Add("User-Agent",
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
-                    "AppleWebKit/537.36 (KHTML, like Gecko) " +
-                    "Chrome/94.0.4606.31 Safari/537.36 " +
-                    "maikon/1.0.0 (Konata Project)");
-
-                // Append request header
-                if (header != null)
-                {
-                    foreach (var (k, v) in header)
-                        request.Headers.Add(k, v);
-                }
-            }
-
-            // Open response stream
-            var response = await request.GetResponseAsync();
-            {
-                // length limitation
-                if (limitLen != 0)
-                {
-                    // Get content length
-                    var totalLen = int.Parse
-                        (response.Headers["Content-Length"]!);
-
-                    // Decline streaming transport
-                    if (totalLen > limitLen || totalLen == 0) return null;
-                }
-
-                // Receive the response data
-                var stream = response.GetResponseStream();
-                try
-                {
-                    await using var memStream = new MemoryStream();
-                    // Copy the stream
-                    if (stream != null)
-                    {
-                        await stream.CopyToAsync(memStream);
-                        // Close
-                        response.Close();
-                        return memStream.ToArray();
-                    }
-
-                    return null;
-                }
-                catch (Exception e)
-                {
-                    return null;
-                }
-            }
-        }
-
         public static async Task<string> HttpGetContent(string url, Dictionary<string, string> headers = null)
         {
             var client = new HttpClient();
