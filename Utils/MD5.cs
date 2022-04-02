2260703752836ad5b5e50c6c6f19a7de52225270
siscon <1748329745@qq.com>
2022-04-02 23:17:22 +0800

    Update MD5.cs

--- Utils/MD5.cs
+++ Utils/MD5.cs
@@ -12,8 +12,6 @@ namespace MalodyInfoQuery.Utils
             string byte2String = null;
             for (int i = 0; i < targetData.Length; i++)
             {
-                //这个是很常见的错误，你字节转换成字符串的时候要保证是2位宽度啊，某个字节为0转换成字符串的时候必须是00的，否则就会丢失位数啊。不仅是0，1～9也一样。
-                //byte2String += targetData[i].ToString("x");//这个会丢失
                 byte2String = byte2String+ targetData[i].ToString("x2");
             }
             return byte2String;
